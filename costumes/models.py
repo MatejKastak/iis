2c1a625cf528c175e17094c6caf16eb35b8b052f
Matej Kastak <matej.kastak@gmail.com>
2018-10-13 15:18:54 +0200

    Basic models for db and testing
    - CostumeTemplate, Accessory and Customer

--- costumes/models.py
+++ costumes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# vzor_kostymu
+class CostumeTemplate(models.Model):
+    costume_type = models.CharField(max_length=100)
+    manufacturer = models.CharField(max_length=100)
+    material = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+
+# doplnok
+class Accessory(models.Model):
+    name = models.CharField(max_length=100)
+    manufactured = models.DateTimeField('date created')
+    description = models.CharField(max_length=500)
+
+# klient
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    second_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    # TODO: is CharField the right type for this variable?
+    tel_num = models.CharField(max_length=10)
--- iis/settings.py
+++ iis/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'costumes.apps.CostumesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
