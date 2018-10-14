4aaa43aa51e965969d6ea665aa551502d289b39c
Matej Kastak <matej.kastak@gmail.com>
2018-10-14 15:17:09 +0200

    All Models from DB implemented in models.py
    - Maybe to __str__(self) won't work because of foreign keys

--- costumes/admin.py
+++ costumes/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 
-# from .models import * ??
-from .models import CostumeTemplate, Accessory, Customer
+from .models import *
 
 admin.site.register(CostumeTemplate)
 admin.site.register(Accessory)
 admin.site.register(Customer)
+admin.site.register(Employee)
+admin.site.register(Store)
+admin.site.register(EmployeeAtStore)
+admin.site.register(Manager)
+admin.site.register(Costume)
+admin.site.register(Borrowing)
+admin.site.register(AccessoryToBorrowing)
+admin.site.register(CostumeToBorrowing)
+admin.site.register(AccessoryToCostumeTemplate)
--- costumes/models.py
+++ costumes/models.py
@@ -9,6 +9,7 @@ class CostumeTemplate(models.Model):
 
     def __str__(self):
         return 'CostumeTemplate = ' + 'costume_type: ' + self.costume_type + 'manufacturer: ' + self.manufacturer + 'material: ' + self.material + 'description: ' + self.description
+
 # doplnok
 class Accessory(models.Model):
     name = models.CharField(max_length=100)
@@ -20,6 +21,7 @@ class Accessory(models.Model):
 
 # klient
 class Customer(models.Model):
+    # TODO: kliets should have logins? Prob uniq
     first_name = models.CharField(max_length=100)
     second_name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
@@ -29,3 +31,89 @@ class Customer(models.Model):
 
     def __str__(self):
         return 'Customer = ' + 'first_name: ' + self.first_name + 'second_name: ' + self.second_name + 'address: ' + self.address + 'email: ' + self.email + 'tel_num: ' + self.tel_num
+
+# zamestnanec
+class Employee(models.Model):
+    # TODO: r_cislo as ID?
+    login = models.CharField(max_length=100)
+    # TODO: check django authentication
+    password = models.CharField(max_length=100) # TODO: Too long?
+    first_name = models.CharField(max_length=100)
+    second_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return 'Employee = ' + 'login: ' + self.login + 'password: ' + self.password + 'first_name: ' + self.first_name + 'second_name: ' + self.second_name
+
+# pobocka
+class Store(models.Model):
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+
+    def __str__(self):
+        return 'Store = ' + 'street: ' + self.street + 'city: ' + self.city
+
+# pracovnik na pobocke
+class EmployeeAtStore(models.Model):
+    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    store_id = models.ForeignKey(Store, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'EmployeeAtStore = ' + 'employee_id: ' + self.employee_id + 'store_id: ' + self.store_id
+
+# spravca
+class Manager(models.Model): # TODO: Manager, idk if Admin/Administrator would colide with something internal in django?
+    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    email = models.CharField(max_length=100)
+    tel_num = models.CharField(max_length=10)
+
+    def __str__(self):
+        return 'Manager = ' + ' employee_id: ' + self.employee_id + 'email: ' + self.email + 'tel_num: ' + self.tel_num
+
+# kostym
+class Costume(models.Model):
+    color = models.CharField(max_length=100)
+    size = models.CharField(max_length=100) # TODO: 100 chars?
+    manufactured = models.DateTimeField('date created')
+    wear_out = models.CharField(max_length=100)
+    employee_manage = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    costume_template = models.ForeignKey(CostumeTemplate, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Costume = ' + 'color: ' + self.color + 'size: ' + self.size + 'manufactured: ' + str(self.manufactured) + 'wear_out: ' + self.wear_out + 'employee_manage: ' + self.employee_manage + 'costume_template: ' + self.costume_template
+
+# vypozicka
+class Borrowing(models.Model):
+    event = models.CharField(max_length=100)
+    borrowed_date = models.DateTimeField('date borrowed')
+    return_date = models.DateTimeField('date to return')
+    borrowing_expiration = models.DateTimeField('?') # TODO: ?
+    final_price = models.IntegerField(default=0)
+    employee_borrowed = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Borrowing = ' + 'event: ' + self.event + 'borrowed_date: ' + str(self.borrowed_date) + 'return_date: ' + str(self.return_date) + 'borrowing_expiration: ' + str(self.borrowing_expiration) + 'final_price: ' + self.final_price + 'employee_borrowed: ' + self.employee_borrowed + 'customer: ' + self.customer
+
+# doplnok_vztahuje_sa_k
+class AccessoryToBorrowing(models.Model):
+    accessory_id = models.ForeignKey(Accessory, on_delete=models.CASCADE)
+    borrowing_id = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'AccessoryToBorrowing = ' + 'accessory_id: ' + self.accessory_id + 'borrowing_id: ' + self.borrowing_id
+
+# kostym_vztahuje_sa_k
+class CostumeToBorrowing(models.Model):
+    costume_id = models.ForeignKey(Costume, on_delete=models.CASCADE)
+    borrowing_id = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'CostumeToBorrowing = ' + 'costume_id: ' + self.costume_id + 'borrowing_id: ' + self.borrowing_id
+
+# patri_k
+class AccessoryToCostumeTemplate(models.Model):
+    accessory_id = models.ForeignKey(Accessory, on_delete=models.CASCADE)
+    costume_template_id = models.ForeignKey(CostumeTemplate, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'AccessoryToCostumeTemplate = ' + 'accessory_id: ' + self.accessory_id + 'costume_template_id: ' + self.costume_template_id
