71ee6307b163db03329aa8e42605262c3e938537
Matej Kastak <matej.kastak@gmail.com>
2018-10-15 20:51:51 +0200

    Default routes for each view based on the shared doc.

--- costumes/urls.py
+++ costumes/urls.py
@@ -4,5 +4,16 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('gallery', views.index, name='gallery')
+    path('gallery', views.index, name='gallery'),
+    path('costume/<int:costume_id>', views.costume, name='costume'),
+    path('costume', views.costume_gallery, name='costume_gallery'),
+    path('accessory/<int:accessory_id>', views.accessory, name='accessory'),
+    path('accessory', views.accessory_gallery, name='accessory_gallery'),
+    path('basket', views.basket, name='backet'),
+    path('user', views.user, name='user'),
+    path('settings', views.settings, name='setting'),
+    path('borrowings', views.borrowings_gallery, name='borrowings_gallery'),
+    path('borrowings/<int:borrowing_id>', views.borrowings, name='borrowings'),
+    path('stores', views.stores_gallery, name='stores_gallery'),
+    path('stores/<int:store_id>', views.stores, name='stores'),
 ]
--- costumes/views.py
+++ costumes/views.py
@@ -1,4 +1,38 @@
+from django.http import Http404
 from django.shortcuts import render
 
 def index(request):
     return render(request, 'costumes/index.html')
+
+def costume(request, costume_id):
+    raise Http404('NOT YET IMPLEMENTED')
+
+def costume_gallery(request):
+    raise Http404('NOT YET IMPLEMENTED')
+
+def accessory(request, accessory_id):
+    raise Http404('NOT YET IMPLEMENTED')
+
+def accessory_gallery(request):
+    raise Http404('NOT YET IMPLEMENTED')
+
+def basket(request):
+    raise Http404('NOT YET IMPLEMENTED')
+
+def user(request):
+    raise Http404('NOT YET IMPLEMENTED')
+
+def settings(request):
+    raise Http404('NOT YET IMPLEMENTED')
+
+def borrowings_gallery(request):
+    raise Http404('NOT YET IMPLEMENTED')
+
+def borrowings(request, borrowing_id):
+    raise Http404('NOT YET IMPLEMENTED')
+
+def stores_gallery(request):
+    raise Http404('NOT YET IMPLEMENTED')
+
+def stores(request, store_id):
+    raise Http404('NOT YET IMPLEMENTED')
