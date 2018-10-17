bc937581717d9b1e6e6bd25a554a89a87e89d781
Matej Kastak <matej.kastak@gmail.com>
2018-10-17 13:26:01 +0200

    Accessory template with views.py handling.

--- costumes/views.py
+++ costumes/views.py
@@ -4,20 +4,22 @@ from django.shortcuts import render, get_object_or_404
 from .models import *
 
 def index(request):
-    context = {'costumes': Costume.objects}
+    context = {'costumes': Costume.objects, 'accessories': Accessory.objects}
     return render(request, 'costumes/index.html', context)
 
 def costumes(request, costume_id):
     raise Http404('NOT YET IMPLEMENTED')
 
 def costumes_gallery(request):
-    raise Http404('NOT YET IMPLEMENTED')
+    context = {'costumes': Costume.objects}
+    return render(request, 'costumes/index.html', context)
 
 def accessories(request, accessory_id):
     raise Http404('NOT YET IMPLEMENTED')
 
 def accessories_gallery(request):
-    raise Http404('NOT YET IMPLEMENTED')
+    context = {'accessories': Accessory.objects}
+    return render(request, 'costumes/index.html', context)
 
 def basket(request):
     raise Http404('NOT YET IMPLEMENTED')
