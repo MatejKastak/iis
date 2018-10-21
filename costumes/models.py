d15055a599c9e952c3d78e5a154448105d0185e2
Matej Kastak <matej.kastak@gmail.com>
2018-10-21 13:54:37 +0200

    Accessory ManyToMany relationship to CostumeTemplate
    - this is displayed in the accessory detail, it shows the costumes the
      accessory belongs to.

--- costumes/models.py
+++ costumes/models.py
@@ -18,6 +18,7 @@ class Accessory(models.Model):
     description = models.CharField(max_length=500)
     picture = models.ImageField(upload_to='accessory_images/', default='default_images/default.png')
     price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)], default=1)
+    belongs_to_costume = models.ManyToManyField(CostumeTemplate)
 
     def __str__(self):
         return 'Accessory = ' + ' name: ' + self.name + ' manufactured: ' + str(self.manufactured) + ' description: ' + self.description
